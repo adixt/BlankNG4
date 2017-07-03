0f3c49e88ecc8731022a51e58984980907eb9277
Adam Dziomdziora <adam.dziomdziora@comarch.com>
2017-07-03 15:07:43 +0200

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { CollapseModule } from 'ngx-bootstrap';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { RootRouteMappingConfiguration } from 'app/app.routing';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CollapseModule.forRoot(),
+    RouterModule.forRoot(RootRouteMappingConfiguration)
   ],
   providers: [],
   bootstrap: [AppComponent]
